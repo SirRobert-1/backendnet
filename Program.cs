54b0ceb29f7c15734c17bf42d87e28a13c842c5b
SirRobert-1 <rviverosgonzalez@gmail.com>
2024-05-01 17:44:15 -0600

    Update CORS policy to include additional origins

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ builder.Services.AddCors(options =>
 options.AddDefaultPolicy(
     policy =>
     {
-        policy.WithOrigins("http://localhost:3001", "http://localhost:3002", "http://localhost:3003", "http://localhost:8080")
+        policy.WithOrigins("http://localhost:3000", "http://localhost:3001", "http://localhost:3002", "http://localhost:3003", "http://localhost:8080")
         .AllowAnyHeader()
         .WithMethods("GET", "POST", "PUT", "DELETE");
     });
